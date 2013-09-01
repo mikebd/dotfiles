1911780d767c76aa26bf4d71556786d1613c985e
Michael Ben-David <michael@localhost>
2013-09-01 14:04:32 -0400

    less: default - do not wrap long lines

--- include/profile.d/less.sh
+++ include/profile.d/less.sh
@@ -1,1 +1,1 @@
-export LESS="--ignore-case --LINE-NUMBERS --RAW-CONTROL-CHARS --quit-if-one-screen --status-column --tabs=4"
+export LESS="--chop-long-lines --hilite-search --ignore-case --LINE-NUMBERS --RAW-CONTROL-CHARS --quit-if-one-screen --status-column --tabs=4"
