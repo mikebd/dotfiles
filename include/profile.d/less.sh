865e0488311971a5d61b24dab0e0a0d223332b15
Michael Ben-David <michael@localhost>
2013-09-30 19:02:07 +0000

    Fix less not displaying any output if only one screen to show

--- include/profile.d/less.sh
+++ include/profile.d/less.sh
@@ -1,1 +1,1 @@
-export LESS="--chop-long-lines --hilite-search --ignore-case --LINE-NUMBERS --RAW-CONTROL-CHARS --quit-if-one-screen --status-column --tabs=4"
+export LESS="--chop-long-lines --hilite-search --ignore-case --LINE-NUMBERS --RAW-CONTROL-CHARS --quit-if-one-screen --no-init --status-column --tabs=4"
